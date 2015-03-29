cbf234bf9f8faebed156cd547e962fa13defa843
Ivanov Alexander <aliaksandr.ivanou@ext.accedo.tv>
2015-03-29 11:22:14 +0200

    add append, replace uri

--- src/uriHandler.py
+++ src/uriHandler.py
@@ -2,8 +2,15 @@ __author__ = 'aliaksandr'
 
 import re
 
+#TODO: implement validation error
+
 
 class UriHandler:
+    """
+    Retrieves the authority from the URI, as described in RFC3986,
+     Authority consists of userinformation@host:port and placed after scheme
+    """
+
     def getAuthority(self, uri):
         scheme = self.getScheme(uri)
         schemeSeparator = self.getSchemeSeparator(uri)
@@ -17,6 +24,11 @@ class UriHandler:
             return match[:-1]
         return match
 
+    """
+    Retrieves the host from the authority. The host is one of: IPv4, IPv6 or domain name.
+    The port can be set ater the host, e.g.: testdomain:9000
+    """
+
     def getHost(self, uri):
         auth = self.getAuthority(uri)
         userInf = self.getUserInformation(uri)
@@ -27,10 +39,34 @@ class UriHandler:
             host = auth
         else:
             host = auth[len(userInf) + 1:]
+        if host.startswith('['):
+            endIndex = host.find(']')
+            if endIndex == -1:
+                return ''
+            return host[1:endIndex]
         colon = host.find(':')
         if colon == -1:
             return host
         return host[:colon]
+
+    def __hasIpV6Host(self, uri):
+        auth = self.getAuthority(uri)
+        userInf = self.getUserInformation(uri)
+        if not auth:
+            #throw error
+            return ''
+        if not userInf:
+            host = auth
+        else:
+            host = auth[len(userInf) + 1:]
+        return host.startswith('[')
+
+    """
+    Gets the user information from the uri.
+    As described in RFC3986, user information is first part of the authority,
+    separated by the '@' symbol.
+    Example: testname@testdomain.ext => testname is a user information
+    """
 
     def getUserInformation(self, uri):
         auth = self.getAuthority(uri)
@@ -42,6 +78,11 @@ class UriHandler:
             return ''
         return auth[:infIndex]
 
+    """
+        retrieves tne port from the URI
+        the port is separated with ':' symbol.
+        also supports the IPv6 format
+    """
     def getPort(self, uri):
         auth = self.getAuthority(uri)
         host = self.getHost(uri)
@@ -50,17 +91,29 @@ class UriHandler:
             return ''
         if userInf:
             userInf += '@'
-        port = auth[(len(userInf) + len(host)):]
+        if self.__hasIpV6Host(uri):
+            port = auth[(len(userInf) + len(host)+2):]
+        else:
+            port = auth[(len(userInf) + len(host)):]
         if port.startswith(':'):
             return port[1:]
         return port
 
+    """
+        retrieves and returnes scheme of the uri,
+        if no scheme specified returns empty string
+        Accorting to RFC3986, scheme is a string until delimiter(which is ';' or '://')
+    """
     def getScheme(self, uri):
         match = re.search('^[\w\+\.-]+:', uri.strip())
         if not match:
             return ""
         return match.group(0)[:-1]
 
+    """
+        returns the separator between sheme and authority,
+        if no scheme specified returns empty string
+    """
     def getSchemeSeparator(self, uri):
         scheme = self.getScheme(uri)
         if not scheme:
@@ -70,6 +123,12 @@ class UriHandler:
             return separator + '//'
         return separator
 
+    """
+        returns the path of the URI
+        the path starts directly after authority with the symbol '/' and ends
+        when with '?,#, or end string'
+        if no uri specified, returns empty string
+    """
     def getPath(self, uri):
         if not uri:
             return ''
@@ -79,14 +138,22 @@ class UriHandler:
         path = uri[(len(auth) + len(scheme) + len(schemeSeparator)):]
         match = re.search('^[^#\?]+([#\?]|$)', path)
         if not match:
-            #throw error?
+            #Todo: throw an error
             return ''
         path = match.group(0)
         if path[-1] in ['?', '#']:
             return path[:-1]
         return path
 
-    def getQuery(self, uri):
+    """
+        Returns the query part of URI as a string.
+        if no URI specified returns empty string
+        if no query found in URI returns empty string
+        otherwise returns query.
+        According to RFC3986, the query follows after the path(which can be empty),
+            it starts with '?' and finised with '# or end of the string'
+    """
+    def getStringQuery(self, uri):
         if not uri:
             return ''
         scheme = self.getScheme(uri)
@@ -97,20 +164,164 @@ class UriHandler:
         query = uri[len(path):]
         if not query:
             return ''
-        if not qu
-        return query
+        match = re.search('^\?([^#]+)([#]|$)', query)
+        if not match:
+            return ''
+        return match.group(1)
 
-    def getFragment(sel, uri):
-        pass
+    """
+        Returns query as a dictionary
+        if no query is specified, returns empty string
+        According to RFC3986, the query follows after the path(which can be empty),
+            it starts with '?' and finised with '# or end of the string'
+    """
+    def getQuery(self, uri):
+        query = self.getStringQuery(uri)
+        if not query:
+            return {}
+        params = {}
+        for part in re.split('[;&]', query):
+        #TODO: cach exception
+            (key, value) = part.split('=')
+            params[key] = value
+        return params
 
-    def normalize(self, uri, domain):
-        pass
+    """
+        Returns the fragment or empty string
+    """
+    def getFragment(self, uri):
+        match = re.search('#([\w]*)', uri)
+        if not match:
+            return ''
+        return match.group(1)
 
-    def appendQuery(self, uri, params):
+    def normalize(self, uri, authority='', scheme=''):
         pass
 
     def getAbsoluteURI(self, uri, domain):
         pass
+
+
+    """
+        Appends scheme to the current URI.
+        if empty scheme , appends nothing
+        Second argument is optional separator, default is '://'
+    """
+    def appendScheme(self, uri, scheme, separator="://"):
+        currScheme = self.getScheme(uri)
+        if not currScheme:
+            return scheme + separator + uri
+            #throw error?
+        return uri
+
+    """
+        Appends authority to the uri if URI has no authority,
+        otherwise doing nothing
+    """
+    def appendAuthority(self, uri, auth):
+        currAuthority = self.getAuthority(uri)
+        if not currAuthority:
+            scheme = self.getScheme(uri)
+            separator = self.getSchemeSeparator(uri)
+            return scheme + separator + auth + uri[len(scheme + separator):]
+        return uri
+
+
+    """
+        Appends path to the URI.
+        If path was already specified, adds new path to the current path
+    """
+    def appendPath(self, uri, path):
+        currPath = self.getPath(uri)
+        newPath = currPath + path
+        partialUri = self.getScheme(uri) + self.getSchemeSeparator(uri) + self.getAuthority(uri)
+        return partialUri + newPath + uri[(len(partialUri) + len(currPath)):]
+
+    """
+        appends query to the uri
+        params is a dictionary of values
+        delimiter is an optional parameter that delimits the query pairs
+    """
+    def appendQuery(self, uri, params, delimiter='&'):
+        query = self.queryToString(params, delimiter)
+        currQuery = self.getStringQuery(uri)
+        partialUri = self.getScheme(uri) + self.getSchemeSeparator(uri) + self.getAuthority(uri) + self.getPath(uri)
+        if currQuery:
+            query = '?' + currQuery + delimiter + query
+            postfixPosition = len(partialUri) + len(currQuery) + 1
+        else:
+            query = '?' + query
+            postfixPosition = len(partialUri)
+        return partialUri + query + uri[postfixPosition:]
+
+    """
+        Appends fragment to the URI
+    """
+    def appendFragment(self, uri, fragment):
+        if not self.getFragment(uri):
+            return uri + '#' + fragment
+        return uri + fragment
+
+    def replaceScheme(self, uri, newScheme, schemeSeparator="://"):
+        currScheme = self.getScheme(uri)
+        currSeparator = self.getSchemeSeparator(uri)
+        newUri = uri[(len(currScheme) + len(currSeparator)):]
+        if not newScheme:
+            return newUri
+        return newScheme + schemeSeparator + newUri
+
+    def replaceAuthority(self, uri, newAuth):
+        currScheme = self.getSchemeWithSeparator(uri)
+        currAuth = self.getAuthority(uri)
+        return currScheme + newAuth + uri[(len(currScheme) + len(currAuth)):]
+
+    def replacePath(self, uri, newPath):
+        currPath = self.getPath(uri)
+        currScheme = self.getSchemeWithSeparator(uri)
+        currAuth = self.getAuthority(uri)
+        return currScheme + currAuth + newPath + uri[(len(currScheme) + len(currAuth) + len(currPath)):]
+
+    def replaceQuery(self, uri, params):
+        newQuery = self.queryToStringWithSeparator(params)
+        currPath = self.getPath(uri)
+        currScheme = self.getSchemeWithSeparator(uri)
+        currAuth = self.getAuthority(uri)
+
+        return currScheme + currAuth + currPath + newQuery + self.getFragmentWithSeparator(uri)
+
+    def buildUri(self, scheme='', shcemeSeparator='://', auth='', path='', query='', fragment=''):
+        return scheme + shcemeSeparator + auth + path + '?' + query + '#' + fragment
+
+    def buildAuthority(self, userInf, host, port):
+        auth = ''
+        if userInf:
+            auth = auth + userInf + '@'
+        if host:
+            auth = auth + host
+        if port:
+            auth = auth + ':' + port
+        return auth
+
+    def queryToString(self, query, delimiter='&'):
+        qString = ''
+        for key in query:
+            qString += key + '=' + str(query[key]) + delimiter
+        return qString[:-1]
+
+    def getSchemeWithSeparator(self, uri):
+        return self.getScheme(uri) + self.getSchemeSeparator(uri)
+
+    def getFragmentWithSeparator(self, uri):
+        fr = self.getFragment(uri)
+        if not fr:
+            return ''
+        return '#' + fr
+
+    def queryToStringWithSeparator(self, params, delimiter='&'):
+        query = self.queryToString(params)
+        if not query:
+            return ''
+        return '?' + query
 
 
 def main():
--- tests/uriHandlerTests.py
+++ tests/uriHandlerTests.py
@@ -18,7 +18,6 @@ class TestUriHandler(unittest.TestCase):
         scheme = self.uriHandler.getSchemeSeparator('http')
         self.assertEqual(scheme, "")
 
-
     def test_getHttpScheme(self):
         scheme = self.uriHandler.getScheme("http://testdomain.com")
         self.assertEqual(scheme, "http")
@@ -39,6 +38,8 @@ class TestUriHandler(unittest.TestCase):
         scheme = self.uriHandler.getScheme("      http ://netrnternetn     ")
         self.assertTrue(not scheme)
         scheme = self.uriHandler.getScheme("http")
+        self.assertTrue(not scheme)
+        scheme = self.uriHandler.getScheme("htt p")
         self.assertTrue(not scheme)
 
     def test_getFtpScheme(self):
@@ -78,6 +79,15 @@ class TestUriHandler(unittest.TestCase):
         self.assertEqual(auth, "127.0.0.1")
         auth = self.uriHandler.getAuthority("http://127.0.0.1:9000/path")
         self.assertEqual(auth, "127.0.0.1:9000")
+        auth = self.uriHandler.getAuthority("http://[xh:ns:s2:32]:9000/path")
+        self.assertEqual(auth, "[xh:ns:s2:32]:9000")
+        auth = self.uriHandler.getAuthority(
+            "http://errors.angularjs.org/1.2.28/$compile/tpload?p0=%2Fpartials%2Ftiles%2Ftile-m-02.html")
+        self.assertEqual(auth, "errors.angularjs.org")
+
+    def getIpV6Authority(self):
+        auth = self.uriHandler.getAuthority('http://[FEDC:BA98:7654:3210:FEDC:BA98:7654:3210]:80/index.html')
+        self.assertEqual(auth,'[FEDC:BA98:7654:3210:FEDC:BA98:7654:3210]:80')
 
     def test_getAuthorityWithUserInf(self):
         auth = self.uriHandler.getAuthority("scheme:myusername:mypassword@domain.com:9000/path")
@@ -108,6 +118,15 @@ class TestUriHandler(unittest.TestCase):
         inf = self.uriHandler.getUserInformation('testemail@testdomain.com')
         self.assertEqual(inf, 'testemail')
 
+    def test_getIpV6UserInformation(self):
+        usefInf = self.uriHandler.getUserInformation('http://test@[FEDC:BA98:7654:3210:FEDC:BA98:7654:3210]:80/index.html')
+        self.assertEqual(usefInf, 'test')
+        usefInf = self.uriHandler.getUserInformation('http://[FEDC:BA98:7654:3210:FEDC:BA98:7654:3210]:80/index.html')
+        self.assertEqual(usefInf, '')
+        usefInf = self.uriHandler.getUserInformation('http://@[FEDC:BA98:7654:3210:FEDC:BA98:7654:3210]:80/index.html')
+        self.assertEqual(usefInf, '')
+
+
     def test_getEmptyHostEmptyRequest(self):
         host = self.uriHandler.getHost('')
         self.assertTrue(not host)
@@ -128,6 +147,11 @@ class TestUriHandler(unittest.TestCase):
         host = self.uriHandler.getHost('http://127.0.0.1:9000?query=12')
         self.assertEqual(host, '127.0.0.1')
 
+    def test_getIpV6Host(self):
+        host = self.uriHandler.getHost('http://[FEDC:BA98:7654:3210:FEDC:BA98:7654:3210]:80/index.html')
+        self.assertEqual(host,'FEDC:BA98:7654:3210:FEDC:BA98:7654:3210')
+
+
     def test_getHostInAuthority(self):
         host = self.uriHandler.getHost("scheme:MYTESTNAME@domain.com/path")
         self.assertEqual(host, "domain.com")
@@ -143,6 +167,10 @@ class TestUriHandler(unittest.TestCase):
         self.assertEqual(host, "9000")
         host = self.uriHandler.getPort("scheme:myusername@DOMAINdomain.com:9000/path")
         self.assertEqual(host, "9000")
+
+    def test_getIpV6Port(self):
+        port = self.uriHandler.getPort('http://[FEDC:BA98:7654:3210:FEDC:BA98:7654:3210]:80/index.html')
+        self.assertEqual(port, '80')
 
     def test_getEmptyPath(self):
         path = self.uriHandler.getPath('')
@@ -173,6 +201,9 @@ class TestUriHandler(unittest.TestCase):
         self.assertEqual(path, '/root/child')
         path = self.uriHandler.getPath('scheme:/root/child')
         self.assertEqual(path, '/root/child')
+        path = self.uriHandler.getPath(
+            "http://errors.angularjs.org/1.2.28/$compile/tpload?p0=%2Fpartials%2Ftiles%2Ftile-m-02.html")
+        self.assertEqual(path, "/1.2.28/$compile/tpload")
 
     def test_getPathQuery(self):
         path = self.uriHandler.getPath('scheme://domain.com/root/child?query')
@@ -185,6 +216,154 @@ class TestUriHandler(unittest.TestCase):
         self.assertEqual(path, '/root/child')
         path = self.uriHandler.getPath('scheme://domain.com/root/child/file.ext#fragment')
         self.assertEqual(path, '/root/child/file.ext')
+
+    def test_getEmptyStringQuery(self):
+        query = self.uriHandler.getStringQuery('scheme://domain.com/root/child#fragment')
+        self.assertTrue(not query)
+        query = self.uriHandler.getStringQuery('scheme://domain.com/root/child/file.ext#fragment')
+        self.assertTrue(not query)
+
+    def test_getStringQuery(self):
+        query = self.uriHandler.getStringQuery('scheme:///root?p1=v1&p2=v2&p3=v3#fragment')
+        self.assertEqual(query, 'p1=v1&p2=v2&p3=v3')
+        query = self.uriHandler.getStringQuery('/root?p1=v1&p2=v2&p3=v3#fragment')
+        self.assertEqual(query, 'p1=v1&p2=v2&p3=v3')
+        query = self.uriHandler.getStringQuery('scheme://domain.com/root?p1=v1&p2=v2&p3=v3#fragment')
+        self.assertEqual(query, 'p1=v1&p2=v2&p3=v3')
+        query = self.uriHandler.getStringQuery('scheme://domain.com/root/child/file.ext?p1=v1&p2=v2&p3=v3#fragment')
+        self.assertEqual(query, 'p1=v1&p2=v2&p3=v3')
+        query = self.uriHandler.getStringQuery('root?p1=v1&p2=v2&p3=v3#fragment')
+        self.assertEqual(query, 'p1=v1&p2=v2&p3=v3')
+        query = self.uriHandler.getStringQuery('scheme:usdata@domain:port/root?p1=v1&p2=v2&p3=v3#fragment')
+        self.assertEqual(query, 'p1=v1&p2=v2&p3=v3')
+        query = self.uriHandler.getStringQuery(
+            "http://errors.angularjs.org/1.2.28/$compile/tpload?p0=%2Fpartials%2Ftiles%2Ftile-m-02.html")
+        self.assertEqual(query, "p0=%2Fpartials%2Ftiles%2Ftile-m-02.html")
+
+    def test_getEmptyQuery(self):
+        query = self.uriHandler.getQuery('scheme://domain.com/root/child#fragment')
+        self.assertTrue(not query)
+        query = self.uriHandler.getQuery('scheme://domain.com/root/child/file.ext#fragment')
+        self.assertTrue(not query)
+
+    def test_getgQuery(self):
+        query = self.uriHandler.getQuery('scheme:///root?p1=v1&p2=v2&p3=v3#fragment')
+        self.assertEqual(query, {'p1': 'v1', 'p2': 'v2', 'p3': 'v3'})
+        query = self.uriHandler.getQuery('/root?p1=v1&p2=v2&p3=v3#fragment')
+        self.assertEqual(query, {'p1': 'v1', 'p2': 'v2', 'p3': 'v3'})
+        query = self.uriHandler.getQuery('scheme://domain.com/root?p1=v1&p2=v2&p3=v3#fragment')
+        self.assertEqual(query, {'p1': 'v1', 'p2': 'v2', 'p3': 'v3'})
+        query = self.uriHandler.getQuery('scheme://domain.com/root/child/file.ext?p1=v1&p2=v2&p3=v3#fragment')
+        self.assertEqual(query, {'p1': 'v1', 'p2': 'v2', 'p3': 'v3'})
+        query = self.uriHandler.getQuery('root?p1=v1&p2=v2&p3=v3#fragment')
+        self.assertEqual(query, {'p1': 'v1', 'p2': 'v2', 'p3': 'v3'})
+        query = self.uriHandler.getQuery('scheme:usdata@domain:port/root?p1=v1&p2=v2&p3=v3#fragment')
+        self.assertEqual(query, {'p1': 'v1', 'p2': 'v2', 'p3': 'v3'})
+
+    def test_getFragment(self):
+        frag = self.uriHandler.getFragment('scheme:///')
+        self.assertEqual(frag, '')
+        frag = self.uriHandler.getFragment('scheme:///#')
+        self.assertEqual(frag, '')
+        frag = self.uriHandler.getFragment('scheme:///root?p1=v1&p2=v2&p3=v3#fragment')
+        self.assertEqual(frag, 'fragment')
+        frag = self.uriHandler.getFragment('scheme:///#fragment')
+        self.assertEqual(frag, 'fragment')
+
+    def test_appendScheme(self):
+        uri = self.uriHandler.appendScheme('', "http")
+        self.assertEqual(uri, 'http://')
+        uri = self.uriHandler.appendScheme('http://', "http")
+        self.assertEqual(uri, 'http://')
+        uri = self.uriHandler.appendScheme('domain.com', "http", ':')
+        self.assertEqual(uri, 'http:domain.com')
+
+    def test_appendAuthority(self):
+        uri = self.uriHandler.appendAuthority("", "domain.com")
+        self.assertEqual(uri, 'domain.com')
+        uri = self.uriHandler.appendAuthority("http://", "domain.com")
+        self.assertEqual(uri, 'http://domain.com')
+        uri = self.uriHandler.appendAuthority("http:///path?query", "domain.com")
+        self.assertEqual(uri, 'http://domain.com/path?query')
+        uri = self.uriHandler.appendAuthority("http://mydomain/path?query", "domain.com")
+        self.assertEqual(uri, 'http://mydomain/path?query')
+
+    def test_appendPath(self):
+        uri = self.uriHandler.appendPath('', '/path')
+        self.assertEqual(uri, '/path')
+        uri = self.uriHandler.appendPath('domain.com', '/path')
+        self.assertEqual(uri, 'domain.com/path')
+        uri = self.uriHandler.appendPath('scheme:', '/path')
+        self.assertEqual(uri, 'scheme:/path')
+        uri = self.uriHandler.appendPath('scheme://domian/root?query#fragment', '/path')
+        self.assertEqual(uri, 'scheme://domian/root/path?query#fragment')
+        uri = self.uriHandler.appendPath('?query', '/path')
+        self.assertEqual(uri, '/path?query')
+
+    def test_appendQuery(self):
+        uri = self.uriHandler.appendQuery('', {'k1': 'v1', 'k2': 'v2'})
+        queryParams = self.uriHandler.getQuery(uri)
+        self.assertEqual(queryParams, {'k1': 'v1', 'k2': 'v2'})
+        self.assertEqual(len(uri), len('?k1=v1&k2=v2'))
+        uri = self.uriHandler.appendQuery('domain.com', {'k1': 'v1', 'k2': 42})
+        queryParams = self.uriHandler.getQuery(uri)
+        self.assertEqual(queryParams, {'k1': 'v1', 'k2': '42'})
+        self.assertEqual(len(uri), len('domain.com?k1=v1&k2=42'))
+        uri = self.uriHandler.appendQuery('domain.com?t1=42&t2=test', {'k1': 'v1', 'k2': 42})
+        queryParams = self.uriHandler.getQuery(uri)
+        self.assertEqual(queryParams, {'k1': 'v1', 'k2': '42', 't1': '42', 't2': 'test'})
+        self.assertEqual(len(uri), len('domain.com?t1=42&t2=test&k1=v1&k2=42'))
+
+    def test_appendFragment(self):
+        uri = self.uriHandler.appendFragment('', 'fragment')
+        self.assertEqual(uri, '#fragment')
+        uri = self.uriHandler.appendFragment('domain', 'fragment')
+        self.assertEqual(uri, 'domain#fragment')
+        uri = self.uriHandler.appendFragment('domain#test', 'fragment')
+        self.assertEqual(uri, 'domain#testfragment')
+
+    def test_replaceScheme(self):
+        uri = self.uriHandler.replaceScheme('', 'http', '://')
+        self.assertEqual(uri, 'http://')
+        uri = self.uriHandler.replaceScheme('scheme://domain.com', 'http', '://')
+        self.assertEqual(uri, 'http://domain.com')
+        uri = self.uriHandler.replaceScheme('scheme:domain.com', 'http', '://')
+        self.assertEqual(uri, 'http://domain.com')
+        uri = self.uriHandler.replaceScheme('scheme:domain.com', '')
+        self.assertEqual(uri, 'domain.com')
+
+    def test_replaceAuthority(self):
+        uri = self.uriHandler.replaceAuthority('domain.com/path?query', 'username@newdomain.net')
+        self.assertEqual(uri, 'username@newdomain.net/path?query')
+        uri = self.uriHandler.replaceAuthority('scheme://domain.com/path?query', 'username@newdomain.net')
+        self.assertEqual(uri, 'scheme://username@newdomain.net/path?query')
+        uri = self.uriHandler.replaceAuthority('/path?query', 'username@newdomain.net')
+        self.assertEqual(uri, 'username@newdomain.net/path?query')
+        uri = self.uriHandler.replaceAuthority('', 'username@newdomain.net')
+        self.assertEqual(uri, 'username@newdomain.net')
+        uri = self.uriHandler.replaceAuthority('scheme:///path', 'username@newdomain.net')
+        self.assertEqual(uri, 'scheme://username@newdomain.net/path')
+        uri = self.uriHandler.replaceAuthority('scheme:///path', '')
+        self.assertEqual(uri, 'scheme:///path')
+
+    def test_replacePath(self):
+        uri = self.uriHandler.replacePath('', '/newpath')
+        self.assertEqual(uri, '/newpath')
+        uri = self.uriHandler.replacePath('scheme://domain/path?query#frag', '/root/newpath')
+        self.assertEqual(uri, 'scheme://domain/root/newpath?query#frag')
+        uri = self.uriHandler.replacePath('scheme://domain/path?query#frag', '')
+        self.assertEqual(uri, 'scheme://domain?query#frag')
+        uri = self.uriHandler.replacePath('scheme://domain/path?query', '')
+        self.assertEqual(uri, 'scheme://domain?query')
+
+    def test_replaceQuery(self):
+        uri = self.uriHandler.replaceQuery('', {'k1': 'v1', 'k2': 42})
+        self.assertEqual(len(uri), len('?k1=v2&k2=42'))
+        uri = self.uriHandler.replaceQuery('scheme://domain/path/path?a=1#fr', {'k1': 'v1', 'k2': 42})
+        self.assertEqual(len(uri), len('scheme://domain/path/path?k1=v2&k2=42#fr'))
+        uri = self.uriHandler.replaceQuery('scheme://domain/path/path?a=1&nn=11#fr', {})
+        self.assertEqual(len(uri), len('scheme://domain/path/path#fr'))
+
 
 # if __name__ == '__main__':
 #     suite = unittest.TestLoader().loadTestsFromTestCase(TestUriHandler)
